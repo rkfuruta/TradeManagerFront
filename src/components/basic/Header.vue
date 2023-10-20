0c51c476e72375198c6f308bba4ed663cafb2bc8
RKFuruta <rcontas@gmail.com>
2023-10-19 22:43:55 -0300

    :lipstick: Refactor header

--- src/components/basic/Header.vue
+++ src/components/basic/Header.vue
@@ -1,19 +1,26 @@
 <template>
-    <div class="nav-bar">
+    <div class="nav-bar" id="topnav">
         <div class="main">
-            <div class="item home" @click="redirect('/')">
-                <span>Home</span>
+            <div class="logo" @click="redirect('/')">
+                <img src="@/assets/logo/eskinbo_primary.svg" alt="logo" />
             </div>
-            <div class="item inventory" @click="redirect('/inventory')">
-                <span>Inventory</span>
+            <div class="item hamburger" @click="open">
+                <img class="hamburger-icon" src="@/assets/icon/align-left.svg" alt="hamburger">
             </div>
-            <div class="item bot" @click="redirect('/bot')">
-                <span>Bot</span>
+            <div class="links">
+                <div class="item inventory" @click="redirect('/inventory')">
+                    <span>Inventory</span>
+                </div>
+                <div class="item bot" @click="redirect('/bot')">
+                    <span>Bot</span>
+                </div>
             </div>
         </div>
         <div class="left">
-            <CurrencySelector />
-            <UserHeader class="user-header item" />
+            <div class="content">
+                <CurrencySelector />
+                <UserHeader class="user-header item" />
+            </div>
         </div>
     </div>
 </template>
@@ -26,6 +33,14 @@ export default  {
     methods: {
         redirect(route) {
             this.$router.push(route);
+        },
+        open() {
+            let x = document.getElementById("topnav");
+            if (x.className === "nav-bar") {
+                x.className += " responsive";
+            } else {
+                x.className = "nav-bar";
+            }
         }
     }
 }
@@ -33,28 +48,73 @@ export default  {
 </script>
 
 <style scoped>
-
 .nav-bar {
-    background-color: #383838;
-    display: flex;
+    background-color: var(--secondary-3);
+    color: white;
     width: 100%;
     justify-content: space-between;
     align-items: center;
-    padding: 0 20px;
-}
-.nav-bar .main {
-    display: flex;
 }
 .nav-bar .item {
     padding: 20px;
     cursor: pointer;
 }
-.nav-bar .item:hover {
-    background-color: #181818;
-}
 .nav-bar .left {
+    position: absolute;
+    top: 0;
+    right: 0;
+}
+.nav-bar .left .content {
     display: flex;
-    justify-content: end;
     align-items: center;
+}
+.hamburger-icon {
+    display: flex;
+    filter: var(--svg-white);
+}
+.nav-bar .main .links {
+    width: 100%;
+    display: flex;
+    flex-wrap: wrap;
+}
+.nav-bar .main .links .item:hover {
+    background-color: var(--secondary-4);
+}
+.nav-bar .logo {
+    cursor: pointer;
+    display: flex;
+    padding-left: 15px;
+    padding-right: 20px;
+}
+.nav-bar .logo img {
+    width: 110px;
+}
+@media screen and (min-width: 601px) {
+    .nav-bar .main .hamburger {
+        display: none;
+    }
+    .nav-bar .main {
+        display: flex;
+    }
+}
+@media screen and (max-width: 600px) {
+    .nav-bar .main .links .item {
+        display: none;
+    }
+    .nav-bar.responsive {
+        position: relative;
+    }
+    .nav-bar.responsive .main .links .item {
+        float: none;
+        display: block;
+        text-align: left;
+    }
+    .nav-bar.responsive .main .links .item:hover {
+        background-color: var(--secondary-4);
+    }
+    .nav-bar.responsive .main .links .item {
+        background-color: var(--secondary-2);
+        width: 100%;
+    }
 }
 </style>
